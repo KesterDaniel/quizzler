d1566fbec0989aee713f39893a174cdc8f2b9db0
KesterDaniel <kesterdan17@gmail.com>
2023-02-17 10:19:20 +0100

    checking answers

--- ui.py
+++ ui.py
@@ -17,17 +17,17 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150, 125,
             text="Word on street",
-            width=250,
-            font=("Arial", 30, "italic")
+            width=280,
+            font=("Arial", 20, "italic")
         )
         self.canvas.grid(row=1, column=0, columnspan=3)
 
         true_image = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0, bd=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, bd=0, command=self.correct_answer)
         self.true_button.grid(row=2, column=0, pady=20)
 
         false_image = PhotoImage(file="./images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0, bd=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, bd=0, command=self.wrong_answer)
         self.false_button.grid(row=2, column=2, pady=20)
 
         self.get_next_question()
@@ -35,6 +35,17 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        next_question = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=next_question)
+        if self.quiz.still_has_questions():
+            next_question = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=next_question)
+        else:
+            self.canvas.itemconfig(self.question_text, text=f"Your final score:\n{self.quiz.score} out of {self.quiz.question_number}")
+
+    def correct_answer(self):
+        self.quiz.check_answer("True")
+        self.get_next_question()
+
+    def wrong_answer(self):
+        self.quiz.check_answer("False")
+        self.get_next_question()
 
