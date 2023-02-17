50fc79f7de5ae95f0091285d014b6db902c9ba3b
KesterDaniel <kesterdan17@gmail.com>
2023-02-17 09:42:24 +0100

    showing the next question

--- ui.py
+++ ui.py
@@ -1,9 +1,11 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
 
-class QuizInterface():
-    def __init__(self):
+class QuizInterface:
+    def __init__(self, quizbrain: QuizBrain):
+        self.quiz = quizbrain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(background=THEME_COLOR, padx=20, pady=20)
@@ -12,7 +14,12 @@ class QuizInterface():
         self.score_label.grid(row=0, column=2, pady=10)
 
         self.canvas = Canvas(width=300, height=250)
-        self.question_text = self.canvas.create_text(150, 125, text="Word on street", font=("Arial", 30, "italic"))
+        self.question_text = self.canvas.create_text(
+            150, 125,
+            text="Word on street",
+            width=250,
+            font=("Arial", 30, "italic")
+        )
         self.canvas.grid(row=1, column=0, columnspan=3)
 
         true_image = PhotoImage(file="./images/true.png")
@@ -23,5 +30,11 @@ class QuizInterface():
         self.false_button = Button(image=false_image, highlightthickness=0, bd=0)
         self.false_button.grid(row=2, column=2, pady=20)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        next_question = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=next_question)
 
